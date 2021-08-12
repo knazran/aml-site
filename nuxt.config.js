8d04c0fb35e0cebb91fc4bcfb69155e89c4b93e6
Knazran <khairulnazran94@gmail.com>
2021-08-12 21:56:11 +0800

    Changed header

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: 'KNazran',
+    title: 'Amirah Mat Lani',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -16,7 +16,7 @@ export default {
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon2.ico' }]
   },
   generate: {
   },
